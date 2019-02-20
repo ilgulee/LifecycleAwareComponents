4cbb31edca4df35db8c5601a5d6617df188519cd
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-20 15:54:24 -0500

    Lifecycle Aware Components lifecycle observer and owner

---
+++ app/src/main/java/ilgulee/com/lifecycleawarecomponents/MainActivityObserver.kt
@@ -0,0 +1,44 @@
+package ilgulee.com.lifecycleawarecomponents
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class MainActivityObserver : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreateEvent() {
+        Log.i(TAG, "Observer ON_create")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStartEvent() {
+        Log.i(TAG, "Observer ON_START")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResumeEvent() {
+        Log.i(TAG, "Observer ON_RESUME")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPauseEvent() {
+        Log.i(TAG, "Observer ON_PAUSE")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStopEvent() {
+        Log.i(TAG, "Observer ON_STOP")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroyEvent() {
+        Log.i(TAG, "Observer ON_DESTROY")
+    }
+
+    companion object : LifecycleObserver {
+        private val TAG: String = MainActivityObserver::class.java.simpleName
+    }
+
+}
